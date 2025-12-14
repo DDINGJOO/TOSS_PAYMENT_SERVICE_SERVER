d4a700a8bad2f849b76255a9b5a9f035d16dee82
ddingjoo <ddingsha9@teambind.co.kr>
2025-12-14 15:22:20 +0900

    REFACTOR: Update KafkaProducerConfig

--- springProject/src/main/java/com/teambind/payment/adapter/out/kafka/config/KafkaProducerConfig.java
+++ springProject/src/main/java/com/teambind/payment/adapter/out/kafka/config/KafkaProducerConfig.java
@@ -15,27 +15,27 @@ import java.util.Map;
 
 @Configuration
 public class KafkaProducerConfig {
-
-    @Value("${spring.kafka.bootstrap-servers}")
-    private String bootstrapServers;
-
-    @Bean
-    public ProducerFactory<String, Object> producerFactory() {
-        Map<String, Object> props = new HashMap<>();
-        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
-        props.put(ProducerConfig.ACKS_CONFIG, "all");
-        props.put(ProducerConfig.RETRIES_CONFIG, 3);
-        props.put(ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, 1);
-        props.put(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, true);
-        props.put(JsonSerializer.ADD_TYPE_INFO_HEADERS, false);
-
-        return new DefaultKafkaProducerFactory<>(props);
-    }
-
-    @Bean
-    public KafkaTemplate<String, Object> kafkaTemplate() {
-        return new KafkaTemplate<>(producerFactory());
-    }
+	
+	@Value("${spring.kafka.bootstrap-servers}")
+	private String bootstrapServers;
+	
+	@Bean
+	public ProducerFactory<String, Object> producerFactory() {
+		Map<String, Object> props = new HashMap<>();
+		props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+		props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+		props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+		props.put(ProducerConfig.ACKS_CONFIG, "all");
+		props.put(ProducerConfig.RETRIES_CONFIG, 3);
+		props.put(ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, 1);
+		props.put(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, true);
+		props.put(JsonSerializer.ADD_TYPE_INFO_HEADERS, false);
+		
+		return new DefaultKafkaProducerFactory<>(props);
+	}
+	
+	@Bean
+	public KafkaTemplate<String, Object> kafkaTemplate() {
+		return new KafkaTemplate<>(producerFactory());
+	}
 }
