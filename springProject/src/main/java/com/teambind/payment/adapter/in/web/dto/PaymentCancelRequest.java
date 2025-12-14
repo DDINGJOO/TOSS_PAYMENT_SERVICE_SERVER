dfb8e721b94c2dca2164cfd4d9854783fc77548f
ddingjoo <ddingsha9@teambind.co.kr>
2025-12-14 15:04:20 +0900

    REFACTOR: Update PaymentCancelRequest DTO

--- springProject/src/main/java/com/teambind/payment/adapter/in/web/dto/PaymentCancelRequest.java
+++ springProject/src/main/java/com/teambind/payment/adapter/in/web/dto/PaymentCancelRequest.java
@@ -3,8 +3,8 @@ package com.teambind.payment.adapter.in.web.dto;
 import jakarta.validation.constraints.NotBlank;
 
 public record PaymentCancelRequest(
-        // 취소 사유
-        @NotBlank(message = "취소 사유는 필수입니다")
-        String reason
+		// 취소 사유
+		@NotBlank(message = "취소 사유는 필수입니다")
+		String reason
 ) {
 }
