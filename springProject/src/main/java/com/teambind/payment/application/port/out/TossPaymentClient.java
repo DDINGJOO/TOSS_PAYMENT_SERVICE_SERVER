467b34bfe873a2823ac17854b862bd85c779a7ce
ddingjoo <ddingsha9@teambind.co.kr>
2025-12-14 15:22:44 +0900

    REFACTOR: Update TossPaymentClient port

--- springProject/src/main/java/com/teambind/payment/application/port/out/TossPaymentClient.java
+++ springProject/src/main/java/com/teambind/payment/application/port/out/TossPaymentClient.java
@@ -4,7 +4,7 @@ import com.teambind.payment.adapter.out.toss.dto.TossPaymentConfirmRequest;
 import com.teambind.payment.adapter.out.toss.dto.TossPaymentConfirmResponse;
 
 public interface TossPaymentClient {
-
-    // 토스 결제 승인 요청
-    TossPaymentConfirmResponse confirmPayment(TossPaymentConfirmRequest request);
+	
+	// 토스 결제 승인 요청
+	TossPaymentConfirmResponse confirmPayment(TossPaymentConfirmRequest request);
 }
